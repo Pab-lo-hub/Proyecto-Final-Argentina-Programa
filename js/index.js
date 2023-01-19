64a2af502679bc238f7cf9758e0b7e9782f23b6b
pab-lo-hub <salaberry.pablo@gmail.com>
2023-01-19 17:38:47 -0300

    connecting Vue

--- js/index.js
+++ js/index.js
@@ -1,8 +1,59 @@
+/**
 $.ajax({
-    url: 'https://randomuser.me/api/',
-    dataType: 'json',
-    success: function(data) {
-      console.log(data);
+  url: 'https://randomuser.me/api/',
+  dataType: 'json',
+  success: function (data) {
+    console.log(data);
+    let persona = data.results[0];
+    console.log(persona);
+  }
+});
+*/
+
+
+const app = Vue.createApp({
+  data() {
+    return {
+      email: "HARDCODEADO",
     }
-  });
-        
+  },
+  methods: {
+    getDataFromRandomUser: function () {
+      $.ajax({
+        url: 'https://randomuser.me/api/',
+        dataType: 'json',
+        success: function (data) {
+          console.log(data);
+          let persona = data.results[0];
+          console.log(persona);
+          this.email = persona.email;
+        }
+      });
+    }
+  }
+})
+
+const { createApp } = Vue
+
+  createApp({
+    data() {
+      return {
+        message: 'Hello Vue!',
+        email: "HARDCODEADO",
+      }
+    },
+    methods: {
+      getDataFromRandomUser: function () {
+        $.ajax({
+          url: 'https://randomuser.me/api/',
+          dataType: 'json',
+          success: function (data) {
+            console.log(data);
+            let persona = data.results[0];
+            console.log(persona);
+            email = persona.email;
+          }
+        });
+      }
+    }
+  }).mount('#app2')
